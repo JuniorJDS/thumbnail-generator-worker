e05352367a071a32c2fc2fbf70941c99244a4be8
Junior Silva <juniorjds2015@gmail.com>
2024-01-23 11:30:52 -0300

    feat: update s3

--- repository/s3.go
+++ repository/s3.go
@@ -1,34 +1,69 @@
 package repository
 
 import (
+	"mime/multipart"
 	"thumbnail-generator-worker/config"
 	infra "thumbnail-generator-worker/infra/aws"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 type S3Repository struct {
-	Uploader *s3manager.Uploader
-	Bucket   string
+	sess   *session.Session
+	Bucket string
 }
 
 var settings = config.GetSettings()
 
 func NewS3Repository() *S3Repository {
-	session := infra.GetSessionAWS()
-	uploader := s3manager.NewUploader(session, func(u *s3manager.Uploader) {
-		u.PartSize = 64 * 1024 * 1024
-	})
 	return &S3Repository{
-		Uploader: uploader,
-		Bucket:   settings["BUCKET"],
+		sess:   infra.GetSessionAWS(),
+		Bucket: settings["BUCKET"],
 	}
 }
 
-func (s *S3Repository) Save() error {
+func (s *S3Repository) Upload(file *multipart.FileHeader, ID string) error {
+	/*
+		f, err := file.Open()
+		if err != nil {
+			return fmt.Errorf("failed to open file: %s", err.Error())
+		}
+		defer f.Close()
+
+		destination := fmt.Sprintf("raw/%s/%s", ID, file.Filename)
+
+		_, err = s.Uploader.Upload(&s3manager.UploadInput{
+			Bucket: aws.String(s.Bucket),
+			Key:    aws.String(destination),
+			Body:   f,
+		})
+
+		if err != nil {
+			return fmt.Errorf("failed to upload file to S3 in Bucket: %s with Error: %s", s.Bucket, err.Error())
+		}
+	*/
 	return nil
 }
 
-func (s *S3Repository) Get() error {
-	return nil
+func (s *S3Repository) Download(ID, fileName string) ([]byte, error) {
+	// TODO: path my-bucket/raw/id/dosimage.svg
+	// TODO: save my-bucket/thumbnail/id/dosimage.svg
+	buf := aws.NewWriteAtBuffer([]byte{})
+
+	downloader := s3manager.NewDownloader(s.sess)
+
+	_, err := downloader.Download(
+		buf, &s3.GetObjectInput{
+			Bucket: aws.String(""),
+			Key:    aws.String(""),
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
 }
